77d3d15e676a2018186807fec988c2185aaa58c6
mykieunguyen <mykieudonguyen@gmail.com>
2023-03-13 21:12:46 -0500

    Add outer div to each store 16 square
    This is for styling purposes

--- javascript.js
+++ javascript.js
@@ -4,13 +4,19 @@ const container = document.querySelector('.container');
 
 // for loop to create columns 
 for (let column = 1; column <= 16; column++) {
-
+    // create outer div to each store 16 squares
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('outerDiv')
+    
     // for loop to create rows 
     for (let row = 1; row <= 16; row++ ) {
         let createDiv = document.createElement('div');
-        createDiv.textContent = 'hello';
+        // Styling
+        createDiv.classList.add('squares');
         createDiv.style.backgroundColor = "red";
-        container.appendChild(createDiv);
+        // Add div to container
+        outerDiv.appendChild(createDiv);
     }
+    container.appendChild(outerDiv);
 }
 
